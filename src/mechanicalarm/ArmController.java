b7ee6d09f696289ca3d7a7c201c5414dfa7012e6
jhenier13 <jhe.nier13@gmail.com>
2010-11-09 21:35:32 -0400

    algunas pruebas agregadas

--- src/mechanicalarm/ArmController.java
+++ src/mechanicalarm/ArmController.java
@@ -2,8 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mechanicalarm;
+
+import java.util.Stack;
+
 
 /**
  *
@@ -11,16 +13,54 @@ package mechanicalarm;
  */
 public class ArmController {
 
+    private Stack<Integer> [] cajas;
     private int numeroDeCajas = 0;
 
-    public int numeroDeCajas(){
+    public int numeroDeCajas() {
         return numeroDeCajas;
     }
 
     public void ingresarCajas(int numeroDeCajas) {
-        if( numeroDeCajas > 0 && numeroDeCajas <= 25  )
+        if (numeroDeCajas > 0 && numeroDeCajas <= 25) {
             this.numeroDeCajas = numeroDeCajas;
-        else
+            cajas = new Stack [this.numeroDeCajas];
+            for (int i = 0; i < this.numeroDeCajas; ++i) {
+                cajas[i] = new Stack<Integer>();
+                cajas[i].push(i);
+            }
+        } else {
             throw new IllegalStateException("El numero de cajas tiene que ser mayor a 0 y menor a 26.");
+        }
+    }
+
+    public void posicionarCaja(int valor, int posicion) {
+        if (posicion < this.numeroDeCajas) {
+            cajas[posicion].push(valor);
+        } else {
+            throw new IllegalArgumentException("La posicion destino no existe");
+        }
+    }
+
+    public int retornarCaja(int posicion) {
+        if(cajas[posicion].empty())
+        {
+            return -1;
+        }
+        else
+        {
+            return cajas[posicion].peek();
+        }
+    }
+
+    public void quitarCaja(int posicion) {
+        if (posicion < this.numeroDeCajas) {
+            cajas[posicion].pop();
+        } else {
+            throw new IllegalArgumentException("La posicion requerida no existe");
+        }
+    }
+
+    public int numeroCajasApiladas(int posicion) {
+        return cajas[posicion].size();
     }
 }
